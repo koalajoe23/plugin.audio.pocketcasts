d3902416817694da7308d6cff9c9de268bd8860e
Tobias Jost <tobiasjost@ksti.de>
2017-01-02 19:41:58 +0100

    Removed buggy kodiswift, added plugin-router

--- addon.py
+++ addon.py
@@ -1,46 +1,48 @@
 # -*- coding: utf-8 -*-
-from kodiswift import Plugin
-import sys
 import os
+import routing
+import sys
+import xbmcgui
+import xbmcplugin
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'resources',
                 'lib', 'python-pocketcasts'))
 from pocketcasts import Api
 
-_plugin = Plugin()
+_plugin = routing.Plugin()
 _api = Api('tobias.jost@spinweb.de', 'd$QdU!jzG79PSc5c')
-
-
-def add_fanart(items):
-    results = []
-    for item in items:
-        new_item = item.copy()
-        new_item['fanart'] = os.path.join(
-                             os.path.dirname(__file__), 'fanart.jpg')
-        results.append(new_item)
-    return results
+_default_fanart = os.path.join(os.path.dirname(__file__), 'fanart.jpg')
 
 
 def podcasts2items(podcasts):
-    items = [{
-        'label': podcast.title,
-        'path': _plugin.url_for('show_episodes', uuid=podcast.uuid),
-        'icon': podcast.thumbnail_url,
-        'thumbnail': podcast.thumbnail_url
-    } for podcast in podcasts]
+    list_items = []
+    for podcast in podcasts:
+        list_item = xbmcgui.ListItem(podcast.title)
+        list_item.setArt({
+            'icon': podcast.thumbnail_url,
+            'thumb': podcast.thumbnail_url,
+            'fanart': _default_fanart
+        })
+        url = _plugin.url_for(show_episodes, uuid=podcast.uuid)
+        list_items.append((url, list_item, True))
 
-    return add_fanart(items)
+    return list_items
 
 
 def episodes2items(episodes):
-    items = [{
-        'label': episode.title,
-        'path': episode.url,
-        'icon': episode.podcast.thumbnail_url,
-        'thumbnail': episode.podcast.thumbnail_url,
-        'is_playable': True
-    } for episode in episodes]
+    list_items = []
+    for episode in episodes:
+        podcast = episode.podcast
+        list_item = xbmcgui.ListItem(episode.title)
+        list_item.setArt({
+            'icon': podcast.thumbnail_url,
+            'thumb': podcast.thumbnail_url,
+            'fanart': _default_fanart
+        })
+        list_item.setProperty('IsPlayable', 'true')
+        url = episode.url
+        list_items.append((url, list_item, False))
 
-    return add_fanart(items)
+    return list_items
 
 
 @_plugin.route('/')
@@ -48,74 +50,108 @@ def index():
     items = [
         {
             'label': 'My Podcasts',
-            'path': _plugin.url_for('my_podcasts')
+            'url': _plugin.url_for(my_podcasts)
         },
         {
             'label': 'New Releases',
-            'path': _plugin.url_for('new_episode_released')
+            'url': _plugin.url_for(new_episode_released)
         },
         {
             'label': 'In Progress',
-            'path': _plugin.url_for('episodes_in_progress')
+            'url': _plugin.url_for(episodes_in_progress)
         },
         {
             'label': 'Starred',
-            'path': _plugin.url_for('starred_episodes')
+            'url': _plugin.url_for(starred_episodes)
         },
         {
             'label': 'Featured',
-            'path': _plugin.url_for('featured_podcasts')
+            'url': _plugin.url_for(featured_podcasts)
         },
         {
             'label': 'Top Charts',
-            'path': _plugin.url_for('popular_podcasts')
+            'url': _plugin.url_for(popular_podcasts)
         },
         {
             'label': 'Trending Now',
-            'path': _plugin.url_for('trending_podcasts')
+            'url': _plugin.url_for(trending_podcasts)
         },
         {
             'label': 'Search Podcast / Add URL',
-            'path': _plugin.url_for('search_podcast')
+            'url': _plugin.url_for(search_podcast)
         }
     ]
 
-    return add_fanart(items)
+    xbmcplugin.setContent(_plugin.handle, 'files')
+    list_items = [
+        (item['url'], xbmcgui.ListItem(item['label']), True) for item in items
+        ]
+    if not xbmcplugin.addDirectoryItems(_plugin.handle, list_items):
+        raise
+    xbmcplugin.endOfDirectory(_plugin.handle)
 
 
 @_plugin.route('/my_podcasts/')
 def my_podcasts():
-    return podcasts2items(_api.my_podcasts())
+    items = podcasts2items(_api.my_podcasts())
+    xbmcplugin.setContent(_plugin.handle, 'albums')
+    if not xbmcplugin.addDirectoryItems(_plugin.handle, items):
+        raise
+    xbmcplugin.endOfDirectory(_plugin.handle, cacheToDisc=False)
 
 
 @_plugin.route('/new_episode_released/')
 def new_episode_released():
-    return episodes2items(_api.new_episodes_released())
+    items = episodes2items(_api.new_episodes_released())
+    xbmcplugin.setContent(_plugin.handle, 'songs')
+    if not xbmcplugin.addDirectoryItems(_plugin.handle, items):
+        raise
+    xbmcplugin.endOfDirectory(_plugin.handle, cacheToDisc=False)
 
 
 @_plugin.route('/episodes_in_progress/')
 def episodes_in_progress():
-    return episodes2items(_api.episodes_in_progress())
+    items = episodes2items(_api.episodes_in_progress())
+    xbmcplugin.setContent(_plugin.handle, 'songs')
+    if not xbmcplugin.addDirectoryItems(_plugin.handle, items):
+        raise
+    xbmcplugin.endOfDirectory(_plugin.handle, cacheToDisc=False)
 
 
 @_plugin.route('/starred_episodes/')
 def starred_episodes():
-    return episodes2items(_api.starred_episodes())
+    items = episodes2items(_api.starred_episodes())
+    xbmcplugin.setContent(_plugin.handle, 'songs')
+    if not xbmcplugin.addDirectoryItems(_plugin.handle, items):
+        raise
+    xbmcplugin.endOfDirectory(_plugin.handle, cacheToDisc=False)
 
 
 @_plugin.route('/featured_podcasts/')
 def featured_podcasts():
-    return podcasts2items(_api.featured_podcasts())
+    items = podcasts2items(_api.featured_podcasts())
+    xbmcplugin.setContent(_plugin.handle, 'albums')
+    if not xbmcplugin.addDirectoryItems(_plugin.handle, items):
+        raise
+    xbmcplugin.endOfDirectory(_plugin.handle, cacheToDisc=False)
 
 
 @_plugin.route('/popular_podcasts/')
 def popular_podcasts():
-    return podcasts2items(_api.popular_podcasts())
+    items = podcasts2items(_api.popular_podcasts())
+    xbmcplugin.setContent(_plugin.handle, 'albums')
+    if not xbmcplugin.addDirectoryItems(_plugin.handle, items):
+        raise
+    xbmcplugin.endOfDirectory(_plugin.handle, cacheToDisc=False)
 
 
 @_plugin.route('/trending_podcasts/')
 def trending_podcasts():
-    return podcasts2items(_api.trending_podcasts())
+    items = podcasts2items(_api.trending_podcasts())
+    xbmcplugin.setContent(_plugin.handle, 'albums')
+    if not xbmcplugin.addDirectoryItems(_plugin.handle, items):
+        raise
+    xbmcplugin.endOfDirectory(_plugin.handle, cacheToDisc=False)
 
 
 @_plugin.route('/search_podcast/')
@@ -126,8 +162,11 @@ def search_podcast():
 @_plugin.route('/podcast/<uuid>/')
 def show_episodes(uuid):
     podcast = _api.podcast(uuid)
-    return episodes2items(podcast.episodes)
-
+    items = episodes2items(podcast.episodes)
+    xbmcplugin.setContent(_plugin.handle, 'songs')
+    if not xbmcplugin.addDirectoryItems(_plugin.handle, items):
+        raise
+    xbmcplugin.endOfDirectory(_plugin.handle, cacheToDisc=False)
 
 if __name__ == '__main__':
     _plugin.run()
